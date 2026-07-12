6b464b9e7d82739d511e874b88cd344f9a2d9921
genie7364-create <genie7364@gmail.com>
2026-07-12 13:04:08 +0900

    created applescript to control safari api

--- scripts/browser_automation.scpt
+++ scripts/browser_automation.scpt
@@ -3,6 +3,18 @@ on run argv
     set targetUrl to item 1 of argv
     set genre to item 2 of argv
 
-    display dialog genre
+    tell application "safari"
+    activate
+
+    set currenttab to current tab of front window
+
+    set current_url to (url of currenttab) as text
+    display dialog current_url
+
+    do javascript "document.title" in currenttab
+    set pagetitle to do javascript "document.title" in currenttab
+    display dialog pagetitle
+
+end tell
 
 end run
--- scripts/safari_controller.sh
+++ scripts/safari_controller.sh
@@ -8,8 +8,9 @@ echo "starting morning automation..."
 # set the target URL to the first argument passed to the script, or use a default URL if no argument is provided. The "${1:-default_value}" syntax means "use the first argument if it exists; otherwise, use 'default_value'".
 target_url="${1:-https://missav123.com/dm298/en/today-hot}"
 "$script_dir/open_safari.sh" "$target_url"
-echo "morning automation complete."
 
+# Get the genre from the second argument passed to the script, or use a default genre if no argument is provided. The "${2:-default_value}" syntax means "use the second argument if it exists; otherwise, use 'default_value'".
 genre="${2:-tit_job}"
-# Run the AppleScript to automate Safari with the specified URL and genre. The osascript command is used to execute AppleScript scripts from the command line. It takes the path to the AppleScript file and any additional arguments that the script requires.
+# Run the AppleScript located in the same directory as this script, passing the target URL and genre as arguments. The osascript command is used to execute AppleScript scripts from the command line.
 osascript "$script_dir/browser_automation.scpt" "$target_url" "$genre"
+echo "morning automation complete."
