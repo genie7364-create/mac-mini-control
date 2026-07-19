3a7e9d2e1f9dcae0dc40d7007f5b094940e2ca33
genie7364-create <genie7364@gmail.com>
2026-07-19 14:46:07 +0900

    javascript api updated in applescript

--- scripts/browser_automation.scpt
+++ scripts/browser_automation.scpt
@@ -1,20 +1,21 @@
 on run argv
 
     set targetUrl to item 1 of argv
-    set genre to item 2 of argv
+    set searchquery to item 2 of argv
 
     tell application "safari"
     activate
-
+    delay 5 
     set currenttab to current tab of front window
 
     set current_url to (url of currenttab) as text
-    display dialog current_url
 
-    
-    set pagetitle to do javascript "document.title" in currenttab
-    display dialog pagetitle
-
+   set jsCode to "const searchInput = document.querySelector('input');"
+   set jsCode to jsCode & "const searchButton = document.querySelector('.ytSearchboxComponentSearchButton');"
+   set jsCode to jsCode & "searchInput.focus();"
+   set jsCode to jsCode & "searchInput.value = '" & searchquery & "';"
+   do JavaScript jsCode in currenttab
+   
 end tell
 
 end run
--- scripts/safari_controller.sh
+++ scripts/safari_controller.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 set -euo pipefail
 
-# Get the directory of the current script.  $0 is the path to the script being executed, and dirname extracts the directory part of that path. The cd command changes to that directory, and pwd prints the absolute path of the current directory. This ensures that we have the correct path to the script regardless of where it is called from.
 script_dir="$(cd "$(dirname "$0")" && pwd)"
-# Run the open_safari.sh script 
+
 echo "starting morning automation..."
-# set the target URL to the first argument passed to the script, or use a default URL if no argument is provided. The "${1:-default_value}" syntax means "use the first argument if it exists; otherwise, use 'default_value'".
-target_url="${1:-https://missav123.com/dm265/en}"
+
+target_url="${1:-https://youtube.com}"
 "$script_dir/open_safari.sh" "$target_url"
 
-# Get the genre from the second argument passed to the script, or use a default genre if no argument is provided. The "${2:-default_value}" syntax means "use the second argument if it exists; otherwise, use 'default_value'".
-genre="${2:-tit_job}"
-# Run the AppleScript located in the same directory as this script, passing the target URL and genre as arguments. The osascript command is used to execute AppleScript scripts from the command line.
-osascript "$script_dir/browser_automation.scpt" "$target_url" "$genre"
+
+searchquery="${2:-4k fancam}"
+
+osascript "$script_dir/browser_automation.scpt" "$target_url" "$searchquery"
 echo "morning automation complete."
